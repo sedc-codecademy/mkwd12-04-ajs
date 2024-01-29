a932fe6457e1f17be50855047cc4e896717fcfc4
tstojanovska <stojanovskatanja03@gmail.com>
2024-01-29 21:01:32 +0100

    Class 08 -promisesDemo

--- G5/Class08/Code/src/promisesDemo.js
+++ G5/Class08/Code/src/promisesDemo.js
@@ -1,13 +1,13 @@
 //simple callbacks example
 
-function simpleFunction(){
-    setTimeout(function(){
+function simpleFunction() {
+    setTimeout(function () {
         console.log("This is the first logged line");
         setTimeout(logSecondMessage, 3000);
     }, 2000);
 }
 
-function logSecondMessage(){
+function logSecondMessage() {
     console.log("This is the second logged line");
 }
 
@@ -25,15 +25,15 @@ function logSecondMessage(){
 //     }
 // }
 
-function waitSomeTime(delayInMiliseconds){
+function waitSomeTime(delayInMiliseconds) {
     //we tell that there is some delayed code in this function
     //it will end eventually, but we don't know how (success, failure)
     return new Promise((resolve, reject) => {
-        if(delayInMiliseconds < 0){
+        if (delayInMiliseconds < 0) {
             reject("The delay can not be negative"); //Promise goes into state Rejected
         }
-        else{
-            setTimeout(function(){
+        else {
+            setTimeout(function () {
                 resolve(`Message after ${delayInMiliseconds} miliseconds`); //Promise goes into state Fulfilled
             }, delayInMiliseconds);
         }
@@ -41,31 +41,31 @@ function waitSomeTime(delayInMiliseconds){
 }
 
 //what we get as response in then or catch is what we passed in resolve/reject
-// waitSomeTime(4000) //Promise is returned after the call
-// .then(function(successResponse){
-//     console.log("Wait some time success:");
-//     console.log(successResponse);
-// })
-// .catch(function(errorResponse){
-//     console.log("Wait some time error:");
-//     console.log(errorResponse);
-// })
-// .finally(function(){
-//     console.log("This message will be logged anyway");
-// })
+waitSomeTime(4000) //Promise is returned after the call
+    .then(function (successResponse) {
+        console.log("Wait some time success:");
+        console.log(successResponse);
+    })
+    .catch(function (errorResponse) {
+        console.log("Wait some time error:");
+        console.log(errorResponse);
+    })
+    .finally(function () {
+        console.log("This message will be logged anyway");
+    })
 
-// waitSomeTime(-4000) //Promise is returned after the call
-// .then(function(successResponse){
-//     console.log("Wait some time success:");
-//     console.log(successResponse);
-// })
-// .catch(function(errorResponse){
-//     console.log("Wait some time error:");
-//     console.log(errorResponse);
-// })
-// .finally(function(){
-//     console.log("This message will be logged anyway");
-// })
+waitSomeTime(-4000) //Promise is returned after the call
+    .then(function (successResponse) {
+        console.log("Wait some time success:");
+        console.log(successResponse);
+    })
+    .catch(function (errorResponse) {
+        console.log("Wait some time error:");
+        console.log(errorResponse);
+    })
+    .finally(function () {
+        console.log("This message will be logged anyway");
+    })
 
 //this line doesn't wait on anything, it is independent and it will be executed as soon as possible
 console.log("Independent message");
@@ -85,35 +85,35 @@ console.log("Independent message");
 // })
 
 //wrap fetch call in a function
-function getDocuments(url){
+function getDocuments(url) {
     return new Promise((resolve, reject) => {
         fetch(url) //Promise
-        .then(function(response){
-            debugger;
-            response.json() //Promise
-            .then(function(data){
-                debugger
-                resolve(data); //Promise goes to fulfilled
+            .then(function (response) {
+                debugger;
+                response.json() //Promise
+                    .then(function (data) {
+                        debugger
+                        resolve(data); //Promise goes to fulfilled
+                    })
             })
-        })
-        .catch(function(error){
-            reject(error); //Promise goes to rejected
-        })
+            .catch(function (error) {
+                reject(error); //Promise goes to rejected
+            })
     })
 }
 
 getDocuments("https://raw.githubusercontent.com/sedc-codecademy/skwd9-04-ajs/main/Samples/documents.json")
-.then(function(documents){
-    //process documents
-    printDocuments(documents);
-})
-.catch(function(error){
-    console.log("error!!");
-    console.log(error);
-})
+    .then(function (documents) {
+        //process documents
+        printDocuments(documents);
+    })
+    .catch(function (error) {
+        console.log("error!!");
+        console.log(error);
+    })
 
-function printDocuments(documents){
-    for(let document of documents){
+function printDocuments(documents) {
+    for (let document of documents) {
         console.log(document);
     }
 }
