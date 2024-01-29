4b4d5f7e32d3980ccd6cc6386442cf852eb4d788
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-01-29 19:34:27 +0100

    documents example done

--- G1/08_async-await/example1/script.js
+++ G1/08_async-await/example1/script.js
@@ -7,6 +7,12 @@ function getDocuments() {
 }
 
 function showDocuments(documents) {
+	documents.forEach(document =>
+		console.log(`${document.name}.${document.type} ${document.size}MB`)
+	);
+}
+
+function checkDocuments(documents) {
 	if (!documents && !Array.isArray(documents)) {
 		throw new Error('Problem with documents!');
 	}
@@ -14,13 +20,55 @@ function showDocuments(documents) {
 	if (!documents.length) {
 		throw new Error('There are no documents');
 	}
-
-	documents.forEach(document =>
-		console.log(`${document.name}.${document.type} ${document.size}MB`)
-	);
 }
 
-getDocuments()
-	.then(res => res.json())
-	.then(body => showDocuments([]))
-	.catch(error => console.log(error));
+function getImportantDocuments(documents) {
+	return new Promise((resolve, reject) => {
+		const importantDocuments = documents.filter(document => document.important);
+		setTimeout(() => {
+			if (!importantDocuments.length) {
+				reject('There are no important documents');
+			}
+			resolve(importantDocuments);
+		}, 1000);
+	});
+}
+
+// Case 1
+// getDocuments()
+// 	.then(res => res.json())
+// 	.then(body => {
+// 		checkDocuments(body);
+// 		showDocuments(body);
+// 	})
+// 	.catch(error => console.log(error));
+
+// Case 2
+// getDocuments()
+// 	.then(res => res.json())
+// 	.then(documents => {
+// 		// documents = null; // to fake the BE response and break the app
+// 		checkDocuments(documents);
+// 		return getImportantDocuments(documents);
+// 	})
+// 	.then(importantDocuments => showDocuments(importantDocuments))
+// 	.catch(err => console.log(err));
+
+// Async - Await
+
+async function handleDocuments() {
+	try {
+		const response = await getDocuments();
+		let body = await response.json();
+		// body = null; // used to break the app
+		checkDocuments(body);
+		const importantDocuments = await getImportantDocuments(body);
+		showDocuments(importantDocuments);
+	} catch (error) {
+		console.log(error);
+	} finally {
+		console.log(`All done at ${new Date().toLocaleString()}`);
+	}
+}
+
+handleDocuments();
