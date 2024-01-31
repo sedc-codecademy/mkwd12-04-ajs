0ee4c9d4a5ddfa06cdd0645eceb032159e6b3cdd
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-01-31 19:21:52 +0100

    hint 2 added

--- G1/08_workshop_2/src/app.js
+++ G1/08_workshop_2/src/app.js
@@ -1,3 +1,13 @@
 const url = 'https://swapi.dev/api/';
-let peopleUrl = `${url}people/?page=<PAGE_NUMBER>`;
-let shipsUrl = `${url}starships/?page=<PAGE_NUMBER>`;
+let peopleUrl = `${url}people/?page=1`;
+let shipsUrl = `${url}starships/?page=1`;
+
+// Getting the people
+fetch(peopleUrl)
+	.then(res => res.json())
+	.then(people => console.log(people));
+
+// Getting the ships
+fetch(shipsUrl)
+	.then(res => res.json())
+	.then(ships => console.log(ships));
